ba54fd38aec8342ae262a2cac7f5f8308cdb71eb
Telesilla3 <160204516+Telesilla3@users.noreply.github.com>
2024-02-16 17:01:00 +0800

    Update randomRoguelike.py

--- randomRoguelike.py
+++ randomRoguelike.py
@@ -1,20 +1,52 @@
-def valid_input(TorF):
-    value = input(TorF)
-    try:
-        int(value)
-        if value.lower == "yes":
-            return True
-        if value.lower == "no":
-            return False
-    except ValueError:
-        print("Invalid input, please try again.")
+player = 
+
+
+Classes = ["Archer", "Barbarian", "Mage"]
+Class_Weapons = {
+    
+    "Archer" : "Bow",
+    "Barbarian" : "Axe",
+    "Mage" : "Wand"
+
+
+}
     
 
+def valid_input(TorF):
+    while True:
+        try:
+            value = input(TorF)
+            if value.lower() == "yes":
+                return True
+            elif value.lower() == "no":
+                return False
+            else:
+                raise ValueError("Invalid input, please type only 'Yes' or 'No'")
+        except ValueError as ve:
+            print(ve)
+def valid_class(cls):
+    while True:
+        try:
+            value = input(cls)
+            if value.lower() == "archer":
+                return value
+            elif value.lower() == "barbarian":
+                return value
+            elif value.lower() == "mage":
+                return value
+            else:
+                raise ValueError("Invalid input, please type only choose amongst the listed classes (Mage, Archer, Barbarian).")
+        except ValueError as ve:
+            print(ve)
+
 player_name = input("What's your name?: ")
+pl
 print(f"Good day to you {player_name}")
 playerdec1 = valid_input("Are you down to fight for good? (input Yes or No): ")
 if playerdec1 == True:
-    print(playerdec1) 
-else:
+    player_class = valid_class("What class will you be in? (Mage, Archer or Barbarian): ")
+elif playerdec1 == False:
     print("Alright. Then another shall take your place.")
     exit
+    
+
